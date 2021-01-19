b681e6e9dffce55b77f9aa6263e4449515f99b04
Rohit Garg <cryptolemy+git@gmail.com>
2021-01-19 08:59:17 -0800

    Making the bot slightly less spammy

--- index.js
+++ index.js
@@ -22,39 +22,37 @@ client.on('message', async msg => {
     const symbols = parsed.reader.getString().split(',');
     const firstSymbol = symbols[0];
     const period = parsed.reader.getString(false);
-    msg.channel.send(`Working...${period && !allowedPeriods.includes(period) ? `with invalid period (${allowedPeriods.join(',')})` : ''}`);
+    const origMessage = await msg.channel.send(`Working...${period && !allowedPeriods.includes(period) ? `with invalid period (${allowedPeriods.join(',')})` : ''}`);
     const generatedUrl = `https://finance.yahoo.com/quote/${firstSymbol}/chart?p=${firstSymbol}#${generateParams({ symbols, period })}`;
     try {
-      await captureWebsite.file(generatedUrl,
-        `./shots/${id}.png`,
-        {
-          hideElements: [
-            '#YDC-UH',
-            '#YDC-Nav',
-            '#mrt-node-Col1-7-Footer',
-            '.stx_chart_controls',
-            '.stx_jump_today',
-          ],
-          fullPage: true,
-          scrollToElement: '.stx-panel-chart',
-          //delay: 1,
-          //emulateDevice: 'iPad',
-          element: '.stx-panel-chart',
-          timeout: 20,
-        });
-      await msg.channel.send('Here you go', {
-        files: [`./shots/${id}.png`]
-      });
-      tiny.shorten(generatedUrl, (res, err) => {
+      tiny.shorten(generatedUrl, async (res, err) => {
         if (err) {
           console.log(err);
         } else {
-          msg.channel.send(`<${res}>`);
+          await captureWebsite.file(generatedUrl,
+            `./shots/${id}.png`,
+            {
+              hideElements: [
+                '#YDC-UH',
+                '#YDC-Nav',
+                '#mrt-node-Col1-7-Footer',
+                '.stx_chart_controls',
+                '.stx_jump_today',
+              ],
+              fullPage: true,
+              scrollToElement: '.stx-panel-chart',
+              //delay: 1,
+              //emulateDevice: 'iPad',
+              element: '.stx-panel-chart',
+              timeout: 20,
+            });
+          await origMessage.edit(`<${res}>`);
+          await msg.channel.send(new Discord.MessageAttachment(`./shots/${id}.png`));
+          fs.unlink(`./shots/${id}.png`, (err) => {
+            if(err) console.error(err);
+          });
         }
       })
-      fs.unlink(`./shots/${id}.png`, (err) => {
-        if(err) console.error(err);
-      });
     } catch (err) {
       console.error(err);
       msg.channel.send(`Seems invalid...${firstSymbol}?`);
